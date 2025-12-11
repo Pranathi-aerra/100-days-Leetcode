b3f4b99e6aa51a13047131f2008ef225e11ac3f6
Pranathi-aerra <pranathiaerra@gmail.com>
2025-12-11 22:02:25 +0530

    Day-4 Product Of array except self

---
+++ Day-4/ProductofArrayExceptSelf.java
@@ -0,0 +1,25 @@
+
+public class ProductofArrayExceptSelf {
+    
+    public int[] productExceptSelf(int[] nums) {
+        int []res=new int[nums.length];
+        int []pre=new int[nums.length];
+        pre[0]=1;
+        int []suf=new int[nums.length];
+        suf[nums.length-1]=1;
+        for(int i=1;i<nums.length;i++)
+        {
+           pre[i]=pre[i-1]*nums[i-1];
+        }
+         for(int i=nums.length-2;i>=0;i--)
+        {
+          suf[i]=suf[i+1]*nums[i+1];
+        }
+        for(int i=0;i<nums.length;i++)
+        {
+            res[i]=pre[i]*suf[i];
+        }
+        return res;
+    }
+}
+
---
+++ Day-4/ProductofArrayExceptSelf.py
@@ -0,0 +1,14 @@
+class ProductofArrayExceptSelf:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res=[1]*len(nums)
+        pre=1
+        for i in range(len(nums)):
+            res[i]=pre
+            pre=pre*nums[i]
+        suf=1;
+        for j in range(len(nums)-1,-1,-1):
+            res[j]*=suf
+            suf=suf*nums[j]
+        return res;
+
+        
