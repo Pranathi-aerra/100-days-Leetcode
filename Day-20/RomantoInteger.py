4b7576936dabc166a94794cce7a7a70e0bc34cc6
Pranathi-aerra <pranathiaerra@gmail.com>
2025-12-27 21:06:44 +0530

    Roman -> Integer using Hashmap

---
+++ Day-20/RomantoInteger.py
@@ -0,0 +1,21 @@
+class RomantoInteger:
+    def romanToInt(self, s: str) -> int:
+        hm={
+            'I':1,
+            'V':5,
+            'X':10,
+            'L':50,
+            'C':100,
+            'D':500,
+            'M':1000
+        }
+        ans=hm[s[-1]]
+        for i in range(len(s)-2,-1,-1):
+
+            if hm[s[i]]<hm[s[i+1]]:
+                ans-=hm[s[i]]
+            else:
+                ans+=hm[s[i]]
+        return ans
+
+        
